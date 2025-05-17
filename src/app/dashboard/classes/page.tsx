6bcd050c36bf9c0f5ddd3c43c41e52dfc700cf0d
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-18 01:01:56 +0700

    Update page.tsx

--- src/app/dashboard/classes/page.tsx
+++ src/app/dashboard/classes/page.tsx
@@ -220,7 +220,7 @@ export default function ClassesPage() {
      ) : classes.length > 0 ? (
        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6">
          {classes.map((classItem) => (
-           <div key={classItem.id} className={`${gradientClass} rounded-xl shadow-sm overflow-hidden`}>
+           <div key={classItem.id} className="bg-blue-500 rounded-xl p-3 sm:p-5 text-white"}>
              <div className="flex justify-between items-start mb-0">
                <div>
                  <h3 className="font-semibold text-lg">{classItem.level}</h3>
