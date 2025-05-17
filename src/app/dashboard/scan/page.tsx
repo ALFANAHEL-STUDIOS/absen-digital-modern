33885ca500603f445044e50fdaf34b77554cf30a
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-18 03:02:06 +0700

    Update page.tsx

--- src/app/dashboard/scan/page.tsx
+++ src/app/dashboard/scan/page.tsx
@@ -498,7 +498,7 @@ export default function ScanQR() {
                   </p>
                   <motion.button
                     onClick={() => setScanning(true)}
-                    className="bg-blue-900 text-white px-6 py-3 rounded-lg hover:bg-orange-500 active:bg-orange-700 transition-colors"
+                    className="bg-blue-800 text-white px-6 py-3 rounded-lg hover:bg-orange-500 active:bg-orange-700 transition-colors"
                     whileTap={{ scale: 0.95 }}
                   >
                     <QrCode className="h-5 w-5 inline-block mr-2" /> 
@@ -532,5 +532,5 @@ export default function ScanQR() {
     </div>
   );
 }
-<hr className="border-t border-none mb-5" />
-<hr className="border-t border-none mb-5" />
+<hr className="border-t border-none mb-4" />
+<hr className="border-t border-none mb-2" />
