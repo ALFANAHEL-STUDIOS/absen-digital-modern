2d70f9b24020f6f7c8ede87a1951a7934250b054
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-18 00:36:29 +0700

    Update page.tsx

--- src/app/dashboard/students/page.tsx
+++ src/app/dashboard/students/page.tsx
@@ -263,7 +263,7 @@ export default function Students() {
           </div>
         </div>
       )}
-      
+      <hr className="border-t border-none mb-5" />
       {/* Confirmation Dialog */}
       <ConfirmDialog
         isOpen={deleteDialogOpen}
