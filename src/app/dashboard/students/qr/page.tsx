fed0394fc59de89d95b19472eec8c1a7fa376f41
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-18 01:29:49 +0700

    Update page.tsx

--- src/app/dashboard/students/qr/page.tsx
+++ src/app/dashboard/students/qr/page.tsx
@@ -268,10 +268,10 @@ export default function StudentsQRList() {
               {/* Content */}
               <div className="flex flex-col justify-center items-center flex-grow py-4 mt-2">
                 {/* Student Information - Centered */}
-                <div className="text-center mb-3 mt-1">
+                <div className="text-center mb-2 mt-1">
                   <h3 className="font-bold text-base text-gray-800">{student.name}</h3>
-                  <div className="text-xs mt-1">
-                    <p className="text-gray-600">NIK : {student.nisn}</p>
+                  <div className="text-xs mt-3">
+                    <p className="text-gray-600">NIK {student.nisn}</p>
                     <p className="text-gray-600">{student.class}</p>
                   </div>
                 </div>
