b083d093d5b3abbccc690a150fadac376c5f75ad
ALFANAHEL <lehan.virtual@gmail.com>
2025-05-17 23:59:10 +0700

    Update page.tsx

--- src/app/dashboard/students/[id]/page.tsx
+++ src/app/dashboard/students/[id]/page.tsx
@@ -217,9 +217,9 @@ export default function StudentDetail({ params }: { params: { id: string } }) {
               <p className="text-xl font-bold text-red-600">0%</p>
               <p className="text-xs text-gray-500 mt-1">0/20 hari</p>
             </div>
-          </div>
+          </div>*/}
         </div>
       </div>
-    </div>*/}
+    </div>
   );
 }
